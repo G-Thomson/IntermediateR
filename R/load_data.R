0d204999489455ab49f41543aef4cf051ef28078
Geoffrey Thomson <gtho123@aucklanduni.ac.nz>
2017-06-25 18:33:08 +1200

    Docs

--- R/load_data.R
+++ R/load_data.R
@@ -1,2 +1,75 @@
-#' Flights departing from Houston airports in 2011
+#' airports
 #'
+#' Airport location metadata
+#'
+#' @format A 3,376 x 7 dataframe listing the location of each airport in
+#' the U.S.A
+#'
+#' @source I got this from Hadley Wickam's *Data Manipulation with dplyr*
+#' presentation from June 2014
+"airports"
+
+#' planes
+#'
+#' Plane metadata
+#'
+#' @format A 2853 x 9 dataframe listing the technical capacities of
+#' the planes which flwe out of Houston in 2011
+#'
+#' @source I got this from Hadley Wickam's *Data Manipulation with dplyr*
+#'  presentation from June 2014
+"planes"
+
+#' weather
+#'
+#' Hourly weather data for Houston for the year 2011
+#'
+#' @format A 8723 x 16 dataframe listing the weather data for Houston for
+#' the year 2011.
+#'
+#' @source I got this from Hadley Wickam's *Data Manipulation with dplyr*
+#'  presentation from June 2014
+"weather"
+
+#' df
+#'
+#' A simple example dataset for demonstrating basic *dplyr* functions
+#'
+#' @format A 5 x 2 dataframe of meaningless data to demonstrating basic
+#' *dplyr* functions.
+#'
+#' @source I got this from Hadley Wickam's *Data Manipulation with dplyr*
+#'  presentation from June 2014
+"df"
+
+#' beatles_blue
+#'
+#' A simple example dataset for demonstrating basic *dplyr* functions
+#'
+#' @format A 6 x 2 dataframe of meaningless data to demonstrating basic
+#' *dplyr* join functions based on the Beatles
+#'
+#' @source I got this from Hadley Wickam's *Data Manipulation with dplyr*
+#'  presentation from June 2014
+"beatles_blue"
+
+#' beatles_green
+#'
+#' A simple example dataset for demonstrating basic *dplyr* functions
+#'
+#' @format A 5 x 2 dataframe of meaningless data to demonstrating basic
+#' *dplyr* join functions based on the Beatles
+#'
+#' @source I got this from Hadley Wickam's *Data Manipulation with dplyr*
+#'  presentation from June 2014
+"beatles_green"
+
+#' life_expectancies
+#'
+#' A simple example dataset for demonstrating basic *dplyr* functions
+#'
+#' @format A 142 x 8 dataframe life expectency data for each country from 1957-2007.
+#'
+#' @source I got this from the Gapminder Foundation \url(http://www.gapminder.org/)
+#' via Jenny Bryan's gapminder package \url(https://github.com/jennybc/gapminder)
+"life_expectancies"
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-devtools::install_github("G-Thomson/IntermediateR")
+# Intermediate R
+
+This package contains the data for Geoffrey Thomson's *Intermediate R* workshops (rightnow that is just *Intermediate R: Data Manipulation and Visualisation*). Once you have installed the package simply load the package to access the data.
+
+It will also install the ['tidyverse'](http://tidyverse.org/) package along with some associated data packages.
+
+To install it just type the following into your R console:
+
+```r
+install.packages("devtools")
+devtools::install_github("G-Thomson/IntermediateR", build_vignettes = TRUE)
+```
+
+Exempla code is also avaliable in the form of vignettes:
+
+e.g. 
+
+```r
+
+TBD
+```
+
+
+
